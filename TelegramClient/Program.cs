e42697455e17c1d4473c75f564baebca917303bb
fabianohkd <fabianoconrado@gmail.com>
2017-02-01 23:24:44 -0200

    Incluindo arquivos do projeto

---
+++ TelegramClient/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Configuration;
+using Telegram.Bot;
+
+namespace TelegramClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TelegramBotClient bot = new TelegramBotClient(getToken());
+            var me = bot.GetMeAsync().Result;
+
+            Console.Title = me.Username;
+
+            bot.StartReceiving();
+            var result = bot.SendTextMessageAsync(args[0], args[1]).Result;
+            bot.StopReceiving();
+        }
+
+        static string getToken()
+        {
+            string _token = ConfigurationManager.AppSettings["token"];
+            if (_token == null)
+            {
+                throw new NullReferenceException("token invalido");
+            }
+            return _token;
+        }
+
+    }
+}
